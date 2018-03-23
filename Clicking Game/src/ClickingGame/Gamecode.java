7bdfd2c1692f502a7bd0c6f1d959c6af74e951a4
BT_1N3_14 <BT_1N3_14@BT_1N3_14.bktech.local>
2018-03-23 09:34:34 -0400

    welp

--- Clicking Game/src/ClickingGame/Gamecode.java
+++ Clicking Game/src/ClickingGame/Gamecode.java
@@ -8,24 +8,38 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.scene.text.Text;
 
 public class Gamecode extends Application {
+	private int score = 0;
+	private boolean scoring = true;
+	private lon timeStep;
 	public static void main(String[] args) {
 		launch(args);
 	}
 
 	@Override
 	public void start(Stage primaryStage) {
-		primaryStage.setTitle("Hello World!");
+		primaryStage.setTitle("Click");
 		Button btn = new Button();
-		btn.setText("Say 'Hello World'");
+		Text txt = new Text(10, 0, "Click Score");
 		btn.setOnAction(new EventHandler<ActionEvent>() {
 
 			@Override
 			public void handle(ActionEvent event) {
-				System.out.println("Hello World!");
+				if(scoring)
+				{
+					score++;
+				}
+				
+				else
+				{
+					score--;
+				}
 			}
 		});
+		
+		timeStep = System.nanoTime() + 1000000000L;
 
 		StackPane root = new StackPane();
 		root.getChildren().add(btn);
